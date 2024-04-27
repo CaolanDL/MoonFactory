e330a32f6368677177ad15ae50eba37804f9dce3
Caolan DL <CaolanDL@outlook.com>
2024-04-27 17:59:36 +0100

    Tech tree colour changes

--- Assets/Code/Menu/Tech Tree/TechTreeController.cs
+++ Assets/Code/Menu/Tech Tree/TechTreeController.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TechTreeController : MonoBehaviour
-{ 
+{
+    [SerializeField] TechTreeNode FirstNode;
+
+
+    private void Awake()
+    {
+        //FirstNode.Unlock();
+    }
 }
